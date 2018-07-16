c74c02af9c10efa9ad0d6668c39ab999ef0eab29
edsant1 <edwins10329@gmail.com>
2018-07-15 20:12:38 -0600

    first try of module

--- index.js
+++ index.js
@@ -1,21 +1,70 @@
 var md5 = require( 'md5' );
 
-function PreventDuplicate( options, redisConnection ){
-  this.wait = options.wait || 60000;
-  this.redisConnection = redisConnection;
+function DuplicateCheck(redisClient, options){
+    if( !options ){
+        options = {};
+    }
+    else if( typeof options !== 'object' ){
+        throw new Error( 'options must be an object' );
+    }
+    else{
+        this.redisClient = redisClient;
+        this.ttl = options.ttl;
+        this.prefix = options.prefix || '';
+        this.redisKeys = options.redisKeys || [ 'method', 'url', 'ip' ];
+    }
 }
 
-PreventDuplicate.prototype.middleware = function( req, res, next ){
-  var reqCopy = JSON.parse( JSON.stringify( req.body ) );
-  // MD5 hash
-  // then check database
-  // then save to database
-  // this.redisConnection.write
-  var hash = md5.hash( req.body );
+DuplicateCheck.prototype.createMiddleware = function( options ){
+    if( !options ){
+        options = this.options;
+    }
+// merge global options to endpoint options
+    return function( req, res, next ){
+        var key = options.prefix;
+        options.redisKeys.forEach( function( item ){
+            key += req[ item ]
+        } );
+        this.stopOtherRequests( key, function(){
+            let hash = this.createHash( req );
+            this.checkHash( key, hash, function( isDuplicate ){
+                if( isDuplicate ){
+                    this.setHash( hash, key, function(){
+                        res.send( 409, { errors: [ 'Duplicate request detected' ] } );
+                    } );
+                }
+                else{
+                    return next();
+                }
+            } );
+        } );
+    }.bind( this )
 }
 
-PreventDuplicate.prototype.retry = function( req, res, next ){
-  process.setImmediate( function(){
-    this.check( req, res, nex );
-  } );
+DuplicateCheck.prototype.stopOtherRequests = function( key, cb ){
+    this.redisClient.getset( key, true, function(){
+        if( previous ){
+            process.setImmediate( function(){
+                this.stopOtherRequests( key, cb );
+            }.bind( this ) );
+        }
+        else{
+            cb();
+        }
+    } );
+}
+
+DuplicateCheck.prototype.createHash = function( req ){
+    var string = JSON.stringify( req.body );
+    return md5( string );
+}
+
+DuplicateCheck.prototype.checkHash = function( key, hash, cb ){
+    this.redisClient.get( key, function(_,dbHash){
+        cb( dbHash === hash );
+    } );
+}
+
+DuplicateCheck.prototype.setHash = function( hash, key, cb ){
+    this.redisClient.setex( key, hash, cb );
 }
